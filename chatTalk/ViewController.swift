2c84b6a568965608e3bfc43bb1cdb055390ae7e8
한병두 <ByungdooHan@hanbyeongduui-macbook-pro.local>
2018-07-08 23:51:12 +0900

    Initial Commit

---
+++ chatTalk/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  chatTalk
+//
+//  Created by 한병두 on 2018. 7. 8..
+//  Copyright © 2018년 Byungdoo Han. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
